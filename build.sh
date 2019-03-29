ae386bdae92b18d989f8f98d9cbe30bb9f9aaaaa
José Morales <jsmsalt@gmail.com>
2019-03-29 18:31:43 -0300

    Update image name

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker login && \
-docker build . -t alpine-java-rpi --no-cache && \
-docker tag alpine-java-rpi jsmsalt/alpine-java-rpi:latest && \
-docker push jsmsalt/alpine-java-rpi:latest
+docker build . -t java-rpi --no-cache && \
+docker tag java-rpi jsmsalt/java-rpi:latest && \
+docker push jsmsalt/java-rpi:latest
